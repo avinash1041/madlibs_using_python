a2e7fb319f3c8a37d0f1a06a1def93f712db1007
avinash1041 <jadhavavinash1041@gmail.com>
2022-11-10 09:41:09 +0530

    final testing

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+#String concatenation using python string methods
+
+# youtube = "Avinash Jadhav python"
+
+# print("subscribe to " + youtube)
+# print("subscribe to {}".format(youtube))
+# print(f"subscribe to {youtube}")
+# print("subscribe to",youtube)
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input("vernb: ")
+famous_person = input("Famous person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+I love to {verb1}. Stay hydrate and {verb2} like you are {famous_person}!"
+
+print(madlib)
